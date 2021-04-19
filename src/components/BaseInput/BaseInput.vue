8335de04b77fe2a94d85ceef387909cd80ce7e31
Jakub Ciepły <jakubc01@gmail.com>
2021-04-20 00:06:43 +0200

    Add BaseInput

---
+++ src/components/BaseInput/BaseInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="base-input">
+    <label class="base-input__label" v-if="label">{{ label }}</label>
+    <input
+      class="base-input__inner"
+      :value="modelValue"
+      :name="name"
+      :id="name"
+      :placeholder="placeholder"
+      @input="$emit('update:modelValue')"
+      :type="type"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    modelValue: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    type: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  emits: ['update:modelValue'],
+};
+</script>
+<style lang="scss" scoped src="./BaseInput.scss" />
